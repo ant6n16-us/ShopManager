fc80478b8ebc687d07a9214cff9fdc8ce6e4a964
ant6n16-us <73589656+ant6n16-us@users.noreply.github.com>
2021-01-12 14:21:57 +0100

    Test para RemoveProduct(String)
    Test desarrollado por mi compañero Jorjimsan al que se le han añadido las modificaciones pedidas.

--- src/test/java/shopManager/TestRemoveProductString.java
+++ src/test/java/shopManager/TestRemoveProductString.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import exceptions.NoEnoughStock;
+import exceptions.NotInBag;
 import exceptions.NotInStock;
 import model.Product;
 import model.Order;
@@ -25,7 +26,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 /**
- * @author Isabel Román, Jorge Jimenez
+ * @author Isabel Román, Jorge Jimenez, Antonio Delgado
  * Clase para realizar los test al método TestRemoveProductString de la clase MyBagManager
  *
  */
@@ -71,46 +72,72 @@ class TestRemoveProductString {
 	
 	/**
 	 * Test method for {@link shopmanager.MyBagManager#removeProduct(java.lang.String)}.
+	 * @throws NotInBag 
 	 */
 	@Test
 	@Tag("unidad")
 	@DisplayName("Prueba del método que elimina un producto")
-	void testRemoveProductString() throws NoEnoughStock, NotInStock {
+	void testRemoveProductString() throws NoEnoughStock, NotInStock, NotInBag {
 		
-		// Añado 1 producto del prodcuto id1
+		// Añado 1 producto del producto id1
 		Mockito.when(producto1Mock.getId()).thenReturn("id1");
 		Mockito.when(producto1Mock.getNumber()).thenReturn(1);
-		// Añado 2 producto del prodcuto id2
+		// Añado 2 productos del producto id2
 		Mockito.when(producto2Mock.getId()).thenReturn("id2");
 		Mockito.when(producto2Mock.getNumber()).thenReturn(2);
+		
+		// Intento configurar que el mock del stock devuelva un producto id1 cuando se busque en el stock
+		// pero no consigo hacerlo. De todos modos funciona correctamente
+		//Mockito.when(stockMock.searchProduct("id1")).thenReturn(producto1Mock);
 		
 		// Meto en la cesta 1 producto 1 y 2 productos 2
 		micestaTesteada.addProduct(producto1Mock);
 		micestaTesteada.addProduct(producto2Mock);
 		
-		// Elimino todo el producto id1 a partir de sus String
+		// Elimino todo el producto id1 a partir de su id
 		micestaTesteada.removeProduct(producto1Mock.getId());
 		
 		// Compruebo que ya no esta en la cesta el producto id1
 		assertTrue(micestaTesteada.findProduct("id1").isEmpty());
 		
-		// Elimino todo el producto id2 a partir de sus String
+		// Elimino todo el producto id2 a partir de su id
 		micestaTesteada.removeProduct(producto2Mock.getId());
 				
-		// Compruebo que ya no esta en la cesta el producto id1
+		// Compruebo que ya no esta en la cesta el producto id2
 		assertTrue(micestaTesteada.findProduct("id2").isEmpty());
 
 		// Caso de borrar un producto con una id que no está en la cesta
 		try {
 			
-			// Intento borrar el producto con id23
+			// Intento borrar el producto con id3
 			micestaTesteada.removeProduct("id3");
 	    	
 			//Salta xcepción así que no debe llegar aquí		    	
-	    	fail("No salta la excepcion NotInStock stock");
-		    }catch(NotInStock e){
-		    	assertEquals("El producto con id id3 no existe en el Stock",e.getMessage(),"El mensaje de la excepci�n no es correcto");
-		    }   
+	    	fail("No salta la excepcion NotInBag");
+		    }catch(NotInBag e){
+		    	assertEquals("El producto con id id3 no existe en la cesta",e.getMessage(),"El mensaje de la excepción no es correcto");
+		    }
+		
+		// Programo el mock del Stock para que devuelva null al buscar un producto con id1
+		Mockito.when(stockMock.searchProduct("id1")).thenReturn(null);
+		Mockito.when(producto1Mock.getId()).thenReturn("id1");
+		
+		// Añado a la cesta un producto id1
+		Mockito.when(producto1Mock.getNumber()).thenReturn(1);
+		micestaTesteada.addProduct(producto1Mock);
+		
+		// Intenta eliminar el producto pero debe gestionar la excepcion NotInStock
+		try {
+			
+			micestaTesteada.removeProduct(producto1Mock.getId());
+
+			//Salta xcepción así que no debe llegar aquí		    	
+	    	fail("No salta la excepción NotInSotck");
+		
+		}catch(NotInStock e) {
+	    	assertEquals("El producto con id id1 no existe en el Stock",e.getMessage(),"El mensaje de la excepción no es correcto");
+		}
+		
 		
 	}
 	
